a54ef8cae4f177439e70ba621dbf691e3fbbb0f3
Theodore Willingham <tsw91905@uga.edu>
2025-07-11 16:21:49 -0400

    finished creating simple chatbot without any tools

--- main.py
+++ main.py
@@ -12,14 +12,31 @@ def main():
     tools = [] # fill with tools our agent can use
     agent_executor = create_react_agent(model, tools)
 
-    print("Welcome! I'm an AI assistant named Timmy created by Theo. Type 'gay' to exit.")
+    print("Welcome! I'm an AI assistant named Timmy created by Theo. Type 'sybau' to exit.")
     print("You can ask me to perform mat hematical calculations or chat with me.")
     
     while True:
         user_input = input("\nYou: ").strip()  #.strip() gets rid of white space infront
         
-        if user_input == "gay":
+        if user_input == "sybau":
             break
         
         print("\nTimmy: ", end="") #this get's rid of the /n which is at the end of a python print by default
+        
+        #How we call the Agent
+        for chunk in agent_executor.stream( #we can stream our LLM's response from the agent_executor
+            {"messages": [HumanMessage(content=user_input)]} # we give our agent input (messages: which is our userinput) HumanMessage rather than system Message
+        ):
+            #Chuncks are parts of responses coming from agent
+            if "agent" in chunk and "messages" in chunk["agent"]: #grab all messages from agent
+                for message in chunk["agent"]["message"]:
+                    print(message.content, end="") # print all message content #steam let's it us see word by word and not just enitre string at once
+        
+        print() #print new line
+    
+if __name__ == "__main__":
+    main()
+                    
+        
+        
         
